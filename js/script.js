1866a76c2f69bf7b60297676dac92bd99db38485
ValerianArdelean <valerian_ardelean@yahoo.com>
2021-05-13 03:36:11 +0100

    test: Testing the output

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 //CREATING THE NAVIGATION/LINK BUTTONS/MENU
 let fragment = document.createDocumentFragment();
+  //creating separate home button
+let home = document.createElement('li');
+home.setAttribute('id', '#link');
+let an = document.createElement('a');
+an.setAttribute('href','#section0');
+an.innerHTML = 'TOP';
+home.appendChild(an);
+fragment.appendChild(home);
+//now the others buttons based on number of sections
 let ul = document.querySelector('ul');
 let i = 1;
 const sections = document.querySelectorAll('section');
@@ -13,7 +22,7 @@ for (section of sections) {
   li.appendChild(a);
   fragment.appendChild(li);
   ul.appendChild(fragment);
-  /* Creating left and right display for content */
+  /* Aligning the content to left and right */
   if (i % 2 === 0) {
     section.setAttribute('class', 'right');
   }
@@ -23,23 +32,17 @@ let nav = document.querySelector('#nav');
 nav.appendChild(fragment);
 // Scroll to section on link click and making the curent section visible
 nav.addEventListener('click', function(e) {
-  /*
-  for (sec of sections) {
-    sec.setAttribute('class', 'none');
-    }
-  */
   e.preventDefault();
-  let link = `#${e.target.textContent}`;
+  let link = `${e.target.hash}`;
+  console.log(link)
   let s = document.querySelector(link.toLowerCase());
-  /*
-  if (e.target.hash.slice(1,5) === 'sect') {
-    s.setAttribute('class', 'active_section');
-  }
-  */
   s.scrollIntoView({behavior: 'smooth'});
 });
 
-// Creating HIGLIGHT MENU as scroling
+
+
+
+//HIGLIGHTING THE BUTTONS MENU as scroling
 window.addEventListener('scroll', function(y) {
   lists = document.querySelectorAll('.activeLi');
   for (list of lists) {
